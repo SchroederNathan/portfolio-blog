e766608ac149b6fb3f689e1ffcbadc2ac810bbb3
“Nathan <schroedernathan2011@icloud.com>
2025-02-27 00:49:06 -0500

    Fixed local build issues

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,8 +4,8 @@ import { ThemeProvider, useTheme } from 'next-themes'
 import { usePathname } from 'next/navigation'
 import { createContext, useEffect, useRef } from 'react'
 
-function usePrevious<T>(value: T) {
-  let ref = useRef<T>()
+function usePrevious<T>(value: T): T | undefined {
+  let ref = useRef<T | undefined>(undefined)
 
   useEffect(() => {
     ref.current = value
@@ -42,7 +42,7 @@ export const AppContext = createContext<{ previousPathname?: string }>({})
 
 export function Providers({ children }: { children: React.ReactNode }) {
   let pathname = usePathname()
-  let previousPathname = usePrevious(pathname)
+  let previousPathname = usePrevious(pathname ?? undefined)
 
   return (
     <AppContext.Provider value={{ previousPathname }}>
