34b1a3202e5b2cf55011c0034d3c24aa6e49432d
“Nathan <schroedernathan2011@icloud.com>
2025-03-02 12:52:29 -0500

    Removed un-used dependancy

--- src/components/ArticleLayout.tsx
+++ src/components/ArticleLayout.tsx
@@ -9,7 +9,6 @@ import { ViewTracker } from '@/components/ViewTracker'
 import { formatDate } from '@/lib/formatDate'
 import { Article } from '@prisma/client'
 import { Prose } from './Prose'
-;import EditArticle from './EditArticle'
 ``
 function ArrowLeftIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
